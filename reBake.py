0b92a20aa6e9275f62e4c4766ee1d1263ce32e53
Grzegorz Czaplik <faflik@onet.pl>
2022-04-23 10:35:02 +0200

    change filename

--- reBake.py
+++ reBake.py
@@ -23,32 +23,42 @@ except:
         file.write("Set correct .env file witch ADDRESS and KEY\n")
     sys.exit()
 
-contract_adres = web3.toChecksumAddress(
-    "0xe2d26507981a4daaaa8040bae1846c14e0fb56bf")
+contract_adres = web3.toChecksumAddress("0xe2d26507981a4daaaa8040bae1846c14e0fb56bf")
 ref = web3.toChecksumAddress("0xf81a4D3e44Daa168cdD9a0b97AA62F528e298858")
 contract = web3.eth.contract(address=contract_adres, abi=abi.ABI)
-miners = contract.functions.getMyMiners(address).call()
 
 
-def reBake():
-    nonce = web3.eth.get_transaction_count(address)
+def accountInfo():
+    global beanRewards, beans, contractBalance
+
     beanRewards = contract.functions.beanRewards(address).call()
     beanRewards = web3.fromWei(beanRewards, 'ether')
     beanRewards = round(beanRewards, 5)
+    
+    beans = contract.functions.getMyMiners(address).call()
+    
+    contractBalance = contract.functions.getBalance().call()
+    contractBalance = web3.fromWei(contractBalance, 'ether')
+    contractBalance = round(contractBalance, 3)
+    
+def reBake():    
+    
+    
+    # nonce = web3.eth.get_transaction_count(address)
+    # tx = contract.functions.hatchEggs(ref).buildTransaction({
+    #     'nonce': nonce,
+    #     'gas': 150000,  # that's enough
+    #     'gasPrice': web3.toWei('5', 'gwei'),
+    # })
 
-    tx = contract.functions.hatchEggs(ref).buildTransaction({
-        'nonce': nonce,
-        'gas': 150000,  # that's enough
-        'gasPrice': web3.toWei('5', 'gwei'),
-    })
-
-    signed_tx = web3.eth.account.sign_transaction(
-        tx, private_key=os.environ['KEY'])
-    txn = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    # signed_tx = web3.eth.account.sign_transaction(
+    #     tx, private_key=os.environ['KEY'])
+    # txn = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
     with open(dir_path + '/log.txt', 'a') as file:
+        timeStamp = time.strftime(format('%d.%m %H:%M'))
         file.write(
-            f"{time.strftime(format('%d.%m %H:%M'))} RE-BAKE {miners} rewards: {beanRewards}\n"
+            f"RE-BAKE: {timeStamp} | Rewards: {beanRewards} | Beans: {beans} | ContracBalance: {contractBalance}\n"
         )
 
 
@@ -56,14 +66,19 @@ def eatBake():
     pass
 
 
-def main():
+def main():    
+    accountInfo()
+    
     if balance < config.MIN_BALANCE:
         with open(dir_path + '/log.txt', 'a') as file:
+            timeStamp = time.strftime(format('%d.%m %H:%M'))
             file.write(
-                f"{time.strftime(format('%d.%m %H:%M'))} Balance too low \n")
+                f"{timeStamp} Balance too low \n")
         sys.exit()
 
     reBake()
+
+
 
 
 if __name__ == "__main__":
