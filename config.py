c311c96ea1921cd4c0721ec7f21885e3b6a15a9b
Grzegorz Czaplik <faflik@onet.pl>
2022-04-24 21:33:57 +0200

    config update

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 """
 suggested crontab settings witch 6:1 rules:
-# bakebeans
 1 */4 * * 1,2,3,4,5,6 python3 /home/USERNAME/beans/reBake.py
 59 23 * * 7 python3 /home/USERNAME/beans/eatBeans.py
 """
